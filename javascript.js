60b35565c1905a5d4c1171c8a6d15a14def4ca16
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2017-02-03 20:42:36 +0200

    Add missing semicolon

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 var youtube_key = config.api_tokens.youtube_access_token;
 var fb_access_token = config.api_tokens.fb_access_token;
 var google_analytics_id = config.google_analytics_id;
-var photopanel = '<div class="col-md-4"><div class="feed-card"><a class="postLink"><span class="postDescription thisClassUsesBlackMagic "></span><img class="postImage feed-img img-responsive" alt=""></a></div></div>'
+var photopanel = '<div class="col-md-4"><div class="feed-card"><a class="postLink"><span class="postDescription thisClassUsesBlackMagic "></span><img class="postImage feed-img img-responsive" alt=""></a></div></div>';
 
 // Google analytics stuff
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
